df973a7152846bf4a2659db252801c251434719a
thyerry <thyerry.nunes@gmail.com>
2023-05-27 18:13:12 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Todo } from '../models/todo.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+	public todos: Todo[] = [];
+	public title: String = 'minha lista';
+	constructor() {
+		this.todos.push(new Todo(1, 'passear com o cachorro', false));
+		this.todos.push(new Todo(2, 'ir para o supermercado', false));
+		this.todos.push(new Todo(3, 'cortar o cabelo', false));
+	}
+
+	remove(todo: Todo) {
+		const index = this.todos.indexOf(todo);
+		if (index !== -1) this.todos.splice(index, 1);
+	}
+	markAsDone(todo: Todo) {
+		todo.done = true;
+	}
+	markAsUndone(todo: Todo) {
+		todo.done = false;
+	}
 }
